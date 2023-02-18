200a176ecaf29e25480579811ff2e17ec7ca86f9
Roopashree-panda <125746754+Roopashree-panda@users.noreply.github.com>
2023-02-18 20:24:01 +0530

    Update streamplit_app.py

--- streamplit_app.py
+++ streamplit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast menu')
-streamlit.text('streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
