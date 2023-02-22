67aec86ae177652034055283ef288a6b40aedbf5
Roopashree-panda <125746754+Roopashree-panda@users.noreply.github.com>
2023-02-22 23:10:28 +0530

    Update streamplit_app.py

--- streamplit_app.py
+++ streamplit_app.py
@@ -73,9 +73,10 @@ def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
    my_cur.execute ("insert into fruit_load_list values ('new_fruit')")
   return "Thanks for adding"+ new_fruit
+streamlit.title('View our fruit list-Add your favourites')
 if streamlit. button( 'Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets[ "snowflake"])
   my_data_rows =get_fruit_load_list()
   my_cnx.close()
   streamlit.dataframe (my_data_rows)
-streamlit.title('View our fruit list-Add your favourites')
+
